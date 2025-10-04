3ea054e9dcad8cbf30c7c75160ed0c975906be76
Sahileweb <sahil2003mundhe@gmail.com>
2025-10-04 21:36:19 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketio=require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+const path = require('path');
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection',(socket)=>{
+    socket.on('sendlocation',(data)=>{
+        io.emit('receivelocation',{id:socket.id,...data});
+});
+socket.on('disconnect',()=>{
+    io.emit('userdisconnected',socket.id);
+})
+});
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+server.listen(3000);
